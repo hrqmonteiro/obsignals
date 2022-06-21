d0408a603ac7c96a9a051cf455420e3615ec5229
hrqmonteiro <hrqmonteiro@protonmail.com>
2022-06-21 00:54:58 -0300

    updates

--- src/components/sections/Download/Download.tsx
+++ src/components/sections/Download/Download.tsx
@@ -12,14 +12,10 @@ export default function About(): JSX.Element {
       <div className='h-full w-full flex flex-wrap'>
         {/* Texts */}
         <div className='w-full md:w-full lg:w-1/2 bg-black flex flex-wrap justify-center items-center px-6 py-20'>
-          <div>
+          <div className='w-full lg:px-10'>
             <h1 data-aos-delay='200' className='font-bold text-5xl mb-10'>
               Download extension <br /> obsignals
             </h1>
-            <div style={{ color: '#b6b6b6' }} className='invisible'>
-              Obsignals is a signal provider for the binary options market, the
-              signals are compatible with various brokers.
-            </div>
             <div className='mb-10'>
               <ChromeButton />
             </div>
